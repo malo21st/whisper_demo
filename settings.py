2f661c5bfd311407603c54a8de09e41297185cc2
malo21st <44739330+malo21st@users.noreply.github.com>
2024-01-28 16:33:47 +0900

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+CAPTION = {
+    'ALL_PROCESS': '**音声**&ensp;:arrow_right:&nbsp;Whisper&nbsp;:arrow_right:&ensp;**文字**&ensp;:arrow_right:&nbsp;ChatGPT&nbsp;:arrow_right:&ensp;**データ**&ensp;:arrow_right:&nbsp;PC&nbsp;:arrow_right:&ensp;**出力**',
+    'AUDIO_TO_TEXT': '**音声**&ensp;:arrow_right:&nbsp;Whisper&nbsp;:arrow_right:&ensp;**文字**',
+    'TEXT_TO_DATA': '**文字**&ensp;:arrow_right:&nbsp;ChatGPT&nbsp;:arrow_right:&ensp;**データ**',
+    'DATA_TO_OUTPUT': '**データ**&ensp;:arrow_right:&nbsp;PC&nbsp;:arrow_right:&ensp;**出力**',
+}
